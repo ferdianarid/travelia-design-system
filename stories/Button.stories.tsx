205665c1910d565a4fcb882f69109e0ddb766a20
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-12 22:58:20 +0700

    update stories storybook

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,38 +1,13 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Button } from '../components/Button';
+import React from "react"
+import { ButtonPrimary, ButtonSecondary } from "../components/Button"
 
 export default {
-	title: 'Example/Button',
-	component: Button,
-	argTypes: {
-		backgroundColor: { control: 'color' },
-	},
-} as ComponentMeta<typeof Button>;
+	title: "Button",
+	component: ButtonPrimary, ButtonSecondary
+}
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+// button primary
+export const PrimaryButton = () => <ButtonPrimary variant="primary">Button Primary</ButtonPrimary>
 
-export const Primary = Template.bind({});
-
-Primary.args = {
-	primary: true,
-	label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-	label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-	size: 'large',
-	label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-	size: 'small',
-	label: 'Button',
-};
+// button secondary
+export const SecondaryButton = () => <ButtonSecondary variant="secondary">Button Secondary</ButtonSecondary>
